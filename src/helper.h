d68ecff72c72370615ee483bfdd55ae6f645dffa
is-tg <devgoattg@gmail.com>
2025-05-20 10:14:23 +0530

    add hex to rgba macro

--- src/helper.h
+++ src/helper.h
@@ -2,6 +2,9 @@
 #define HELPER_H
 
 #include "SDL3/SDL.h"
+
+#define RGBA(hex) ((hex >> 24) & 0xFF), ((hex >> 16) & 0xFF), ((hex >> 8) & 0xFF), (hex & 0xFF)
+
 SDL_AppResult SDL_AppFail(char *report);
 
 #endif
